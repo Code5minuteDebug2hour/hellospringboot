273d21b85c53ba23f4be1a4eacbaa653a9936d5e
zhangxuefeng <zhangxuefeng@365haoyou.com>
2018-12-17 16:22:33 +0800

    init

---
+++ src/main/java/com/zxf/demo/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.zxf.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloWorldController {
+    @RequestMapping(value = "/helloworld",method = RequestMethod.GET)
+    public String hello(){
+        return "hello springboot";
+    }
+}
